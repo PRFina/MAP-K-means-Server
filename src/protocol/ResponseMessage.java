bfb2da5153bce1954129df9e4b55656dff87c75d
Pio Raffaele Fina <pioraffaelefina@outlook.it>
2018-06-29 17:27:38 +0200

    Add Javadoc to ResponseMessage class

--- src/protocol/ResponseMessage.java
+++ src/protocol/ResponseMessage.java
@@ -3,7 +3,35 @@ package protocol;
 import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
-
+/**
+ * This class model a response message sent from server to client .
+ *
+ * <p>
+ *     From Server point of view, an object of
+ *     this class should be created to send
+ *     information to the client like data or some feedback
+ *     if any errors occours at any point in time on the server.
+ * </p>
+ *
+ * <p>
+ *     From Client point of view,
+ *     an object of this class
+ *     shouldn't never be created
+ *     to send information to server,
+ *     instead should always retrieved from socket and
+ *     analyzed to retrieve payload data or server error.
+ * </p>
+ *
+ * A request message is composed of 3 main component:
+ * <ul>
+ *     <li> Request type: signal to the
+ *     client which service has been called</li>
+ *     <li> Status: signal to the client if computation
+ *     have succeed (OK value) or failed ("ERROR")</li>
+ *     <li> Body: contains the payload information
+ *     data or a message if any errors occurs</li>
+ * </ul>
+ */
 public class ResponseMessage implements Serializable {
     private MessageType type;
 
